078fa486348c0a457dedf728bab498bccb39502f
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-08 10:26:50 -0400

    Add chapter 1 exercises

---
+++ chapter01/ex2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+        printf("shelby\nbaker");
+        return 0;
+}
---
+++ chapter01/ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int a = 5, b = 10;
+        double x = 25.5, y = 20;
+
+        printf("Int Sum = %d\n", a + b);
+        printf("Int Product = %d\n", a * b);
+        printf("Float Sum = %f\n", x + y);
+        printf("Float Product = %f\n", x * y);
+        return 0;
+}
