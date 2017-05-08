e9c5589224658a7bdbb4e44ed61788c7777e41e8
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-08 10:40:32 -0400

    Fix dir structure

---
+++ chapter01/ex5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+        printf("H   H  E E E  L      L      O O O\n");
+        printf("H   H  E      L      L      O   O\n");
+        printf("H H H  E E E  L      L      O   O\n");
+        printf("H   H  E      L      L      O   O\n");
+        printf("H   H  E E E  L L L  L L L  O O O\n");
+        return 0;
+}
