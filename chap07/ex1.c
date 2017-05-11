abf20734fd2ae70b8d7f962a59afdff9863ad44c
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-11 13:50:58 -0400

    Add ex1 chapter 7

---
+++ chap07/ex1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char string[]);
+
+int main(void)
+{
+        char line[100];
+        printf("Enter a string to reverse: \n");
+        fgets(line, 100, stdin);
+
+        reverse(line);
+        printf("%s\n", line);
+        return 0;
+}
+
+void reverse(char string[])
+{
+        char rev[100];
+        int len , i;
+
+        for (len = strlen(string) - 1, i = 0 ; len >= 0; len--, i++)
+                rev[i] = string[len];
+
+        string = strcpy(string, rev);
+}
