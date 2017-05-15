21a88fd1be1753c2b267461e68040922acac5b7f
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-15 14:10:12 -0400

    Add ex3 chapter 9

--- chap09/chap9.h
+++ chap09/chap9.h
@@ -8,11 +8,18 @@ struct money {
         int cents;
 };
 
+struct employee {
+        char name[20];
+        double pay;
+        char dept;
+};
+
 /* Function prototypes */
 struct money init(int dollars, int cents);
 struct money add(struct money a, struct money b);
 void add2(struct money a, struct money b, struct money *c);
 struct money input(void);
 void print(struct money mon);
+void sort(struct employee [] array, int sz);
 
 #endif
---
+++ chap09/ex3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+        return 0;
+}
+
+void sort(struct employee [] array, int sz)
+{
+        
+}
