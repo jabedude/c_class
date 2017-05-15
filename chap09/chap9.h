b616becdc8d16f8430ecc8fff597575cf025ad8f
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-15 13:19:22 -0400

    Add ex1 chapter 9

--- chap09/chap9.h
+++ chap09/chap9.h
@@ -3,4 +3,15 @@
 
 /* Structs */
 
+struct money {
+        int dollars;
+        int cents;
+};
+
+/* Function prototypes */
+struct money init(int dollars, int cents);
+struct money add(struct money a, struct money b);
+struct money input(void);
+void print(struct money mon);
+
 #endif
--- chap09/ex1.c
+++ chap09/ex1.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "chap9.h"
 
-int main(int argc, char *argv[])
+int main(void)
 {
-        
+        struct money a = init(5, 10);
+        struct money b, c;
+
+        b = input();
+        c = add(a, b);
+        print(c);
         return 0;
+}
+
+struct money init(int dollars, int cents)
+{
+        struct money ret;
+        ret.dollars = dollars;
+        ret.cents = cents;
+
+        return ret;
+}
+
+struct money input(void)
+{
+        char d_in[4], c_in[4];
+        int dollars, cents;
+        struct money ret;
+
+        printf("Please enter dollars: ");
+        fgets(d_in, sizeof(int), stdin);
+        printf("Please enter cents: ");
+        fgets(c_in, sizeof(int), stdin);
+
+        dollars = atoi(d_in);
+        cents = atoi(c_in);
+
+        ret.dollars = dollars;
+        ret.cents = cents;
+
+        return ret;
+}
+ struct money add(struct money a, struct money b)
+{
+        struct money ret;
+        ret.dollars = a.dollars + b.dollars;
+        ret.cents = a.cents + b.cents;
+
+        return ret;
+}
+
+ void print(struct money a)
+{
+         printf("There is %d dollars and %d cents in this money.\n", a.dollars, a.cents);
 }
