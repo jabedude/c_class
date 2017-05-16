43e01138fc17c878d58b7d7d952242adbc556784
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-16 09:41:44 -0400

    Add sort in ex3 chapter 9

--- chap09/ex3.c
+++ chap09/ex3.c
@@ -7,5 +7,15 @@ int main(void)
 
 void sort(struct employee [] array, int sz)
 {
-        
+        struct employee tmp;
+
+        for (int i = 0; i < sz; i++) {
+                for (int j = i + 1; j < sz; j++) {
+                        if (array[i].pay > array[j].pay) {
+                                tmp = array[i];
+                                array[i] = array[j]
+                                array[j] = tmp;
+                        }
+                }
+        }
 }
