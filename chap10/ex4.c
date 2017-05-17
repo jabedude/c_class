23154f22a2145b2c4e81bab1f0fad4cbe8a65837
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-17 15:02:26 -0400

    Rename variables and fix tabs

--- chap10/ex4.c
+++ chap10/ex4.c
@@ -1,59 +1,54 @@
-/*
-Poor mans wc made by me
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
-/*
-func prototype
-*/
 int counter(FILE* file, char* fname);
 
 int main(int argc, char* argv[])
 {
 
-        if(argc != 2)
-        {
-                printf("Please supply one argument\n");
-                return 1;
-        }
+    if (argc != 2)
+    {
+        printf("Please supply one argument\n");
+        return 1;
+    }
+    
+    FILE* f;
+    f = fopen(argv[1], "r");
+    
+    counter(f, argv[1]);
 
-        FILE* filepointer;
-        filepointer = fopen(argv[1], "r");
-
-        counter(filepointer, argv[1]);
-
-        return 0;
+    fclose(f);
+    
+    return 0;
 }
 
 int counter(FILE* file, char* fname)
 {
-        int count = 0;
-        int wordcount = 0;
-        int charcount = 0;
-        int lastCharAlphaNum = 1;
-        char ch;
+    int count = 0;
+    int word_count = 0;
+    int char_count = 0;
+    int lastCharAlphaNum = 1;
+    char ch;
 
-        while((ch = fgetc(file)) != EOF)
-        {
-                if(ch == '\n')
-		{
-		        count++;
-			wordcount++;
-		}
-                else if(ch == ' ' && lastCharAlphaNum)
-                        wordcount++;
+    while((ch = fgetc(file)) != EOF)
+    {
+        if(ch == '\n')
+	{
+	    count++;
+        word_count++;
+	}
+        else if(ch == ' ' && lastCharAlphaNum)
+            word_count++;
 
-                if( isalpha(ch) || isdigit(ch) )
-                        lastCharAlphaNum = 1;
-                else
-                        lastCharAlphaNum = 0;
+        if( isalpha(ch) || isdigit(ch) )
+            lastCharAlphaNum = 1;
+        else
+            lastCharAlphaNum = 0;
 
-                charcount++;
-        }
+        char_count++;
+    }
 
-        printf("%d\t%d\t%d\t%s\n", count, wordcount, charcount, fname);
-        return count;
+    printf("%d\t%d\t%d\t%s\n", count, word_count, char_count, fname);
+    return count;
 }
