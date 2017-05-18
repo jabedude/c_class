81ae9eef2781b50bbc110b64ea6d51b48825eff3
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-18 14:13:40 -0400

    Fix indents in print statement

--- chap11/ex2.c
+++ chap11/ex2.c
@@ -12,15 +12,15 @@ main(void)
 
     t = localtime(&tm);
 
-    printf("The date is %d/%d/%d\n", t->tm_mday
-                                 , t->tm_mon + 1
-                                 , t->tm_year + 1900);
+    printf("The date is %d/%d/%d\n", t->tm_mday,
+                                     t->tm_mon + 1,
+                                     t->tm_year + 1900);
 
     sleep(10);
 
-    printf("The date is %d/%d/%d\n", t->tm_mday
-                                 , t->tm_mon + 1
-                                 , t->tm_year + 1900);
+    printf("The date is %d/%d/%d\n", t->tm_mday,
+                                     t->tm_mon + 1,
+                                     t->tm_year + 1900);
 
     return 0;
 }
