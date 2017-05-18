03c9603d5971414ff0c688197e2b20fd5fed8daf
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-18 13:59:16 -0400

    Add ex2 chapter 11

---
+++ chap11/ex2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+int 
+main(void)
+{
+    struct tm *t;
+    time_t tm;
+
+    time(&tm);
+
+    t = localtime(&tm);
+
+    printf("The date is %d/%d/%d\n", t->tm_mday
+                                 , t->tm_mon + 1
+                                 , t->tm_year + 1900);
+
+    sleep(10);
+
+    printf("The date is %d/%d/%d\n", t->tm_mday
+                                 , t->tm_mon + 1
+                                 , t->tm_year + 1900);
+
+    return 0;
+}
