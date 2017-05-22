965893f4464e7d5d6653823b678a18688f0af65d
Josh Abraham <sinisterpatrician@gmail.com>
2017-05-22 13:49:10 -0400

    Add dir ex chapter 1

--- intermediate/chap01/dir.c
+++ intermediate/chap01/dir.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 
 
 int
-main (void)
+main (int argc, char **argv)
 {
     DIR *dp;
     struct dirent *ep;
+    struct stat st;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <dir to list>\n", argv[0]);
+        return 1;
+    }
   
-    dp = opendir ("./");
+    dp = opendir(argv[1]);
     if (dp != NULL)
     {
-        while (ep = readdir (dp))
-            puts (ep->d_name);
-        (void) closedir (dp);
+        while (ep = readdir(dp))
+        {
+            stat(ep->d_name, &st);
+            printf("%o\t%ld\t", st.st_mode, st.st_mtim);
+            printf("%s\n", ep->d_name);
+        }
+        (void) closedir(dp);
     }
     else
-        perror ("Couldn't open the directory");
+        perror("Couldn't open the directory");
   
     return 0;
 }
